71fa08a7e3f739ca81acfe553a67c2633f39050c
Nilkumar Patel <nsp4898@gmail.com>
2021-04-12 11:10:20 -0400

    step 3 & 4: All filters populated

--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
 
-$.get('http://randyconnolly.com/funwebdev/services/visits/browsers.php').done(function(data){
-    printData(data);
-})
-
-function printData(data){
-    var temp ='';
-    data.forEach(element => {
-        temp += `<option value='${element.id}'> ${element.name} </option>`; 
+urls = [
+    {
+        url: 'http://randyconnolly.com/funwebdev/services/visits/browsers.php',
+        id: $('#filterBrowser')
+    },
+    {
+        url: 'http://randyconnolly.com/funwebdev/services/visits/os.php',
+        id: $('#filterOS')
+    },
+    {
+        url: 'http://randyconnolly.com/funwebdev/services/visits/countries.php?continent=EU',
+        id: $('#filterCountry')
+    }
+]
+function populateFilters(url, id) {
+    return $.get(url).done(data => {
+        printData(data, id);
     });
-    $('#filterBrowser').append(temp);
+}
+
+function printData(data, id) {
+    var temp = '';
+    data.forEach(element => {
+        temp += `<option value='${element.id}'> ${element.name} </option>`;
+    });
+    id.append(temp);
+}
+
+for (var i = 0; i < urls.length; i++) {
+    populateFilters(urls[i].url, urls[i].id);
 }
